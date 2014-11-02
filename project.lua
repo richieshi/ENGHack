d95902f6d3f5ef8c8fa036bb7a325d5c091dcdff
shinlgami <y72luo@uwaterloo.ca>
2014-11-02 11:32:58 -0500

    change

--- project.lua
+++ project.lua
@@ -2,7 +2,9 @@ scriptId = 'com.project'
 
 locked = true
 unlockedSince = 0 
-turnn = false
+turn = false
+lastTime = 0
+previousTime = 0  
  
 function onForegroundWindowChange(app, title)
 	myo.debug("Title: " .. title)
@@ -13,29 +15,58 @@ function onPoseEdge(pose, edge)
 
 	run = true
 	
+	pose = conditionallySwapWave(pose)
 	if(edge == "on") then
 		if (pose == "thumbToPinky") then
-			toggleLock()
+			previousTime = myo.getTimeMilliseconds()
+			while (myo.getTimeMilliseconds() - previousTime < 3000) do 
+				if (pose == "fingersSpread" and edge == "on") then
+					toggleLock()
+				end
+			end
+			myo.debug("end")
 		end
 		
 		if (not locked) then
-			if (pose == "waveIn") then
-				nextSong()
+			if (pose == "waveIn" ) then
+					nextSong()
 			elseif (pose == "waveOut") then
-				prevSong()
+					prevSong() 
 			elseif (pose == "fist") then
-				play()
+					play()
 			elseif (pose == "fingersSpread") then
-				initYaw = myo.getYaw()
+				initRoll = myo.getRoll()
 				turn = true
 			end			
 		end
 	else
-		if (pose == "fingersSpread") then
-			turn = false
-		end
+		turn = false
 	end
 end
+
+--[[function run(pose)
+	
+	if (previousGesture == pose) then
+		if (timeSet == false) then
+			previousGesture = pose
+			setTime()
+			timeSet = true
+			return false
+		elseif (timeSet == true) then
+			if (myo.getTimeMilliseconds() - previousTime > 500) then
+				return true
+			else
+				return false
+			end
+		end	
+	else 
+		timeSet = false
+	end
+end --]]
+
+function setTime()
+	previousTime = myo.getTimeMilliseconds
+end 
 
 function toggleLock()
 	locked = not locked
@@ -46,6 +77,10 @@ function toggleLock()
 	end
 end
 
+function run() 
+ 
+ end
+ 
 function nextSong()
 	myo.keyboard("right_arrow", "press")
 end
@@ -62,8 +97,40 @@ function adjustVolume()
 	
 end
 
-function onPerodic()
-	if (turn) then
-		myo.debug(myo.getYaw - initYaw)
+function volumeDown()
+	now = myo.getTimeMilliseconds()
+	if (now - lastTime > 250) then
+		myo.keyboard("down_arrow", "press", "control")
+		lastTime = myo.getTimeMilliseconds()
 	end
+end
+
+function volumeUp()
+	now = myo.getTimeMilliseconds()
+	if (now - lastTime > 250) then 
+		myo.keyboard("up_arrow", "press", "control")
+		lastTime = myo.getTimeMilliseconds()
+	end 
+end
+
+function onPeriodic()
+
+	if (turn) then		
+	 	if (myo.getRoll() - initRoll > 0.1) then 
+			volumeUp()
+		elseif (myo.getRoll() - initRoll < -0.1) then 
+			volumeDown() 
+		end
+	end
+end
+
+function conditionallySwapWave(pose)
+	if myo.getArm() == "left" then 
+		if pose == "waveIn" then 
+			pose = "waveOut"
+		elseif pose == "waveOut" then
+			pose = "waveIn"
+		end
+	end
+	return pose
 end
