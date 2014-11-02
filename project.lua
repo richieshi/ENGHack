f7c1ed781ab9bc7239f4d84ca036f971cb1e3622
richieshi <richie_shi@hotmail.com>
2014-11-01 20:47:04 -0400

    Initial commit

---
+++ project.lua
@@ -0,0 +1,46 @@
+scriptId = 'com.project'
+
+mouseControl = false
+locked = false
+scroll = false
+ 
+function onForegroundWindowChange(app, title)
+	myo.debug("Title: " .. title)
+	return title
+end
+
+function onFist()
+	if (mouseControl) then
+		myo.controlMouse(false)
+		mouseControl = false
+	else
+		myo.controlMouse(true)
+		mouseControl = true
+	end
+end
+
+function onWaveIn()
+	myo.mouse("center", "down")
+end
+
+function offWaveIn()
+	myo.keyboard("down_arrow", "up")
+end
+
+function onThumbToPinky()
+	
+end
+
+function onPoseEdge(pose, edge)
+	if (pose == "waveIn") then
+		onWaveIn()
+	else
+		offWaveIn()
+	end
+	
+	if(pose == "fist" and edge == "on") then
+		onFist()
+	elseif (pose == "thumbToPinky" and edge == "on") then
+		onThumbToPinky()
+	end
+end
